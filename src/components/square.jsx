6a07ef2dc22fd554e04d8fe592a0ff49e8e0ad20
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-02-11 13:28:07 -0300

    Add drag and drop functionality to Square component

--- src/components/square.jsx
+++ src/components/square.jsx
@@ -1,13 +1,25 @@
+import { useDrag, useDrop } from "react-dnd";
 import { selectPieceIcon } from "../gameLogic.js/pieces";
 
 
 
 const Square = ({value, isDark }) => {
   const bgColor = isDark ? 'bg-green-500' : 'bg-white';
-  const piece = selectPieceIcon(value)
+
+  const [, drag] = useDrag({
+    type: 'PIECE', // Specify a unique type for your draggable piece
+    item: { value: value }, // Pass any additional data you want to associate with the dragged item
+  });
+
+  const [, drop] = useDrop({
+    accept: 'PIECE', // Specify the type of draggable items that can be dropped
+    drop: (item) => console.log(`Dropped piece with value ${item.value} onto Square with value ${value}`),
+  });
+  
+  const piece = selectPieceIcon(value);
   return (
-    <div className={`border border-gray-500 p-4 text-center ${bgColor}`}>
-      <div className="w-12 h-12">{piece}</div>
+    <div className={`border border-gray-500 p-4 text-center ${bgColor}`} ref={drop} >
+      <div className="w-12 h-12" ref={drag}>{piece}</div>
     </div>
   )
 }
