19d76af068030751424a4fcc34dcaabeeed268cf
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-03-22 19:21:33 -0300

    delete parseBoard. Add transformFen and convertToBoard

--- src/gameLogic/helpers.js
+++ src/gameLogic/helpers.js
@@ -1,17 +1,64 @@
+const initialPostion = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
 const getPieceColor = (piece) => {
   return piece === piece.toLowerCase() ? 'black' : 'white';
 }
 
-function parseBoard(boardString) {
-  const board = [];
-  for (let i = 0; i < 8; i++) {
-      const row = boardString.substr(i * 8, 8);
-      board.push(row.split(''));
-  }
-  return board;
+// const  parseBoard = (boardString) => {
+//   const board = [];
+//   for (let i = 0; i < 8; i++) {
+//       const row = boardString.substr(i * 8, 8);
+//       board.push(row.split(''));
+//   }
+//   return board;
+// }
+
+const transformFen = (fen) => {
+  const result = [];
+  const rows = fen.split('/');
+  rows.forEach((row) => {
+    let temp = [];
+    row.split('').forEach((char) =>{
+      if(isNaN(Number(char)) ){
+        temp.push(char);
+      }
+      else{
+        let val = Number(char);
+        for(let i = 0; i<val; i++){
+          temp.push('0');
+        }
+      }
+    })
+    result.push(temp);
+  })
+  return(result);
 }
+
+const convertToBoard = (fen) => {
+  const result = {
+    pieces: '',
+    turn: '',
+    castling: '',
+    enPassant: '',
+    halfMove: '',
+    fullMove: '',
+  };
+  const splitedFen = fen.split(' ');
+  const board = transformFen(splitedFen[0]);
+
+  result.pieces = board;
+  result.turn = splitedFen[1];
+  result.castling = splitedFen[2];
+  result.enPassant = splitedFen[3];
+  result.halfMove = splitedFen[4];
+  result.fullMove = splitedFen[5];
+  console.log(result)
+  return result
+}
+
+convertToBoard(initialPostion);
 
 module.exports = {
   getPieceColor,
-  parseBoard,
+  convertToBoard,
 };
