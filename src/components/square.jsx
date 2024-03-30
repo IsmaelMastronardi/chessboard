58cd92236a59af37a54b3516e4006fe4805c7e43
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-03-29 21:32:56 -0300

    Refactor Square component to handle piece selection and highlight possible squares

--- src/components/square.jsx
+++ src/components/square.jsx
@@ -1,12 +1,16 @@
 import { useDrag, useDrop } from "react-dnd";
-import { selectPieceIcon } from "../gameLogic.js/pieces";
-import { movePiece } from "../redux/slices/boardSlice";
+import { movePiece, selectPiece } from "../redux/slices/boardSlice";
 import { useDispatch } from "react-redux";
+import { selectPieceIcon } from "../gameLogic/pieces";
 
 
-const Square = ({value, isDark, index }) => {
+const Square = ({value, isDark, index, posibleSquare }) => {
   const bgColor = isDark ? 'bg-green-500' : 'bg-white';
   const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(selectPiece(index));
+  }
 
   const handleDrop = (oldPost, newPos) => {
     dispatch(movePiece(oldPost, newPos));
@@ -25,7 +29,9 @@ const Square = ({value, isDark, index }) => {
   
   const piece = selectPieceIcon(value);
   return (
-    <div className={`border border-gray-500 p-4 text-center ${bgColor}`} ref={drop} >
+    <div className={`border border-gray-500 p-4 text-center relative ${bgColor}`} ref={drop} onClick={handleClick}>
+      {posibleSquare &&
+        <div className="absolute w-6 h-6 bg-gray-300 rounded-full top-7 right-7 opacity-80"></div>}
       <div className="w-12 h-12" ref={drag}>{piece}</div>
     </div>
   )
