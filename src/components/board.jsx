7cd95ef8345c6f6c0a72dde59a25ec34a2f62b13
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-04-01 21:53:02 -0300

    Fix highlighting bug in Board component

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -5,6 +5,7 @@ import { getPosibleMoves } from "../redux/slices/boardSlice";
 
 const Board = () => {
   const {convertedBoard, posibleMoves, selectedPiece} = useSelector((store) => store.gameBoard);
+  console.log(convertedBoard)
   return(
     <section className="flex flex-col items-center gap-10">
       <p>board</p>
@@ -13,7 +14,7 @@ const Board = () => {
         return row.map((square, colIndex) => {
           const isDark = (rowIndex + colIndex) % 2 === 1;
           let highlighted = false;
-          if(selectedPiece && JSON.stringify(posibleMoves[selectedPiece])?.includes(JSON.stringify([rowIndex, colIndex]))){
+          if(selectedPiece && JSON.stringify(posibleMoves[`${selectedPiece[0]}${selectedPiece[1]}`])?.includes(JSON.stringify([rowIndex, colIndex]))){
             highlighted = true;
           }
           else {
@@ -24,7 +25,7 @@ const Board = () => {
               <Square
               value={square}
               isDark={isDark}
-              index={`${rowIndex}${colIndex}`}
+              index={[rowIndex, colIndex]}
               posibleSquare={highlighted}/>
             </div>
           );
