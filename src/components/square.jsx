2ea704507f68586cc8bac6d211b6c72ca6ebd9b1
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-04-01 21:52:57 -0300

    Refactor Square component to validate and handle piece movement

--- src/components/square.jsx
+++ src/components/square.jsx
@@ -1,19 +1,26 @@
 import { useDrag, useDrop } from "react-dnd";
 import { movePiece, selectPiece } from "../redux/slices/boardSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectPieceIcon } from "../gameLogic/pieces";
 
 
 const Square = ({value, isDark, index, posibleSquare }) => {
+  const {posibleMoves, selectedPiece} = useSelector((store) => store.gameBoard);
   const bgColor = isDark ? 'bg-green-500' : 'bg-white';
   const dispatch = useDispatch();
+
+  const validateMove = (newIndex) => {
+    return JSON.stringify(posibleMoves[`${selectedPiece[0]}${selectedPiece[1]}`])?.includes(JSON.stringify([newIndex[0], newIndex[1]]))
+  }
 
   const handleClick = () => {
     dispatch(selectPiece(index));
   }
 
   const handleDrop = (oldPost, newPos) => {
-    dispatch(movePiece(oldPost, newPos));
+    if(validateMove(newPos)){
+      dispatch(movePiece(oldPost, index));
+    }
   }
 
   const [, drag] = useDrag({
@@ -23,8 +30,9 @@ const Square = ({value, isDark, index, posibleSquare }) => {
 
   const [, drop] = useDrop({
     accept: 'PIECE',
-    drop: (item) => 
-    handleDrop(item.value, value),
+    drop: (item) => {
+      handleDrop(item.index, index);
+    } 
   });
   
   const piece = selectPieceIcon(value);
@@ -32,7 +40,7 @@ const Square = ({value, isDark, index, posibleSquare }) => {
     <div className={`border border-gray-500 p-4 text-center relative ${bgColor}`} ref={drop} onClick={handleClick}>
       {posibleSquare &&
         <div className="absolute w-6 h-6 bg-gray-300 rounded-full top-7 right-7 opacity-80"></div>}
-      <div className="w-12 h-12" ref={drag}>{piece}</div>
+      <div className="w-12 h-12" onDragStart={handleClick} ref={drag}>{piece}</div>
     </div>
   )
 }
