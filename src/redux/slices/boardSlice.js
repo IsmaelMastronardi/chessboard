3208db5a1064e25d55906c0985d1bfa1eaa10b39
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-04-03 23:37:54 -0300

    Add convertToFen function and update board state

--- src/redux/slices/boardSlice.js
+++ src/redux/slices/boardSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { calculatePosibleMoves } from "../../gameLogic/generateMoves";
-import { convertToBoard } from "../../gameLogic/helpers";
+import { convertToBoard, convertToFen } from "../../gameLogic/helpers";
 
 export const movePiece = (pieceIndex, newIndex) => (dispatch) => {
   console.log('moving piece')
@@ -12,6 +12,8 @@ export const selectPiece = (index) => (dispatch) => {
 };
 export const getPosibleMoves = (board, color) => (dispatch) => {
   const allyColor = color === 'w' ? 'white' : 'black';
+  console.log(allyColor);
+  console.log('aaaaaaaaa')
   const result = calculatePosibleMoves(board, allyColor);
   dispatch(gameBoardSlice.actions.updatePosibleMoves(result));
 };
@@ -30,12 +32,17 @@ const gameBoardSlice = createSlice({
   initialState,
   reducers: {
     updateBoard: (state, action) => {
-      const oldIndex = action.payload.pieceIndex
-      const newIndex = action.payload.newIndex
-      const piece = state.convertedBoard.pieces[oldIndex[0]][oldIndex[1]]
+      const oldIndex = action.payload.pieceIndex;
+      const newIndex = action.payload.newIndex;
+      const piece = state.convertedBoard.pieces[oldIndex[0]][oldIndex[1]];
       state.convertedBoard.pieces[oldIndex[0]][oldIndex[1]] = '0';
       state.convertedBoard.pieces[newIndex[0]][newIndex[1]] = piece;
-      state.selectedPiece = ''
+      state.convertedBoard.turn === 'w' ? state.convertedBoard.turn = 'b' : state.convertedBoard.turn = 'w';
+      state.selectedPiece = '';
+      state.fenBoard = convertToFen(state.convertedBoard);
+      const moves = calculatePosibleMoves(state.convertedBoard, state.convertedBoard.turn === 'w' ? 'white' : 'black');
+      console.log(moves);
+      state.posibleMoves = moves;
     },
     updateSelectedPiece: (state, action) => {
       state.selectedPiece = action.payload;
