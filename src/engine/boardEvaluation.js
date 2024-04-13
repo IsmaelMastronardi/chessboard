b8196460df8eb44924d91ddafc039f4e87019b82
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-04-12 21:11:38 -0300

    Add boardEvaluation.js file for calculating position value of pieces on the board

---
+++ src/engine/boardEvaluation.js
@@ -0,0 +1,21 @@
+const { convertToBoard, getPieceColor } = require("../gameLogic/helpers");
+const { pieceWeights, pst_w } = require("./pieceValues");
+
+const calculatePositionValue = (board, allyColor) => {
+  let totalValue = 0;
+  board.forEach((row, rowIndex) => {
+    row.forEach((col, colIndex) => {
+      if(getPieceColor(col) === allyColor && col !== '0'){
+        totalValue += pieceWeights[col.toLowerCase()]
+        if(allyColor === 'white'){
+          totalValue += pst_w[col.toLowerCase()][rowIndex][colIndex]
+        }
+      }
+    })
+  })
+  return totalValue
+}
+
+const initialPostion = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1";
+const board = convertToBoard(initialPostion);
+console.log(calculatePositionValue(board.pieces,board.turn === 'w'? 'white' : 'black'));
