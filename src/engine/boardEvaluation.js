3a72492cddc7f87bf1ab9399d2a1b744095d3d9e
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-04-15 19:17:47 -0300

    Refactor minimax function in boardEvaluation.js to improve readability and remove unnecessary console.log statements

--- src/engine/boardEvaluation.js
+++ src/engine/boardEvaluation.js
@@ -23,51 +23,46 @@ const calculatePositionValue = (board, allyColor) => {
 }
 
 const minimax = (board, depth, maximizingPlayer) => {
-  // console.log('depth', depth);
-  // console.log('board', board);
   if (depth === 0) {
     return {
       value: calculatePositionValue(board.pieces, board.turn === 'w' ? 'white' : 'black'),
-      move: null // No move at this stage
+      move: null,
     };
   } else {
     const moves = calculatePosibleMoves(board, board.turn === 'w' ? 'white' : 'black');
     if (moves === 'checkmate') {
       return {
         value: maximizingPlayer ? -Infinity : Infinity,
-        move: null // No move at this stage
+        move: null,
       };
     }
     if (moves === 'stalemate') {
       return {
         value: 0,
-        move: null // No move at this stagen
+        move: null,
       };
     }
     let bestValue = maximizingPlayer ? -Infinity : Infinity;
-    let bestMove = null; // Initialize best move to null
+    let bestMove = null;
     for (const key in moves) {
       for (const move of moves[key]) {
         const newBoard = JSON.parse(JSON.stringify(board));;
-        finalizeMove(newBoard, key, move);
+        finalizeMove(newBoard, key, move.move);
         const { value } = minimax(newBoard, depth - 1, !maximizingPlayer);
         if ((maximizingPlayer && value > bestValue) || (!maximizingPlayer && value < bestValue)) {
           bestValue = value;
-          console.log('move',move, 'value', value)
-          bestMove = { piece: key, move : move.move }; // Store the best move
+          bestMove = { piece: key, move : move.move };
         }
       }
     }
     return {
       value: bestValue,
-      bestMove,
+      piece: bestMove.piece,
+      move: bestMove.move
     };
   }
 };
 
-
-const initialPostion = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-const secondPosition = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 1 1";
-const board = convertToBoard(secondPosition);
-const move  = minimax(board, 4, true);
-console.log(move);
+module.exports = { 
+  minimax
+};
